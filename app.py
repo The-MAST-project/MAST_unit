f87740ecdb91f3b9b6b537d7181f52834ef55bea
Arie Blumenzweig <theblumz@gmail.com>
2023-05-28 18:26:27 +0300

    added openapi schema

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.openapi.utils import get_openapi
 from PlaneWave import pwi4_client
 from unit import Unit
 from utils import init_log, PrettyJSONResponse, HelpResponse
@@ -12,13 +13,13 @@ unit_id = 17
 logger = logging.getLogger('mast')
 init_log(logger)
 
-app = FastAPI()
+app = FastAPI(docs_url='/docs', redocs_url=None, openapi_url='/mast/api/v1/openapi.json')
 pw = pwi4_client.PWI4()
 unit = Unit(unit_id)
 root = '/mast/api/v1/'
 
 
-@app.get(root + 'status', response_class=PrettyJSONResponse)
+@app.get(root + 'planewave/status', response_class=PrettyJSONResponse)
 def pw_status(request: Request):
     return pw.status()
 
@@ -31,6 +32,49 @@ subsystems = {
     'stage': {'obj': unit.stage, 'name': 'unit.stage'},
     'covers': {'obj': unit.covers, 'name': 'unit.covers'}
 }
+
+
+def set_mast_openapi_schema():
+
+    openapi_schema = get_openapi(
+        title="Welcome to the mistery show!",
+        version="1.0",
+        description="This page allows you to explore the MAST Api",
+        routes=app.routes,
+    )
+
+    openapi_schema['servers'] = [{
+        'url': 'http://127.0.0.1:8000/mast/api/v1'
+    }]
+
+    openapi_schema['paths'] = dict()
+    for sub in subsystems.keys():
+        sub_path = '/' + subsystems[sub]['name'].replace('unit.', '')
+        sub_obj = subsystems[sub]['obj']
+        tuples = inspect.getmembers(sub_obj, inspect.ismethod)
+        for tup in tuples:
+            if Mastapi.is_api_method(tup[1]):
+                path = sub_path + '/' + tup[0]
+                method = tup[1]
+                description = method.__doc__ if method.__doc__ is not None else None
+                openapi_schema['paths'][path] = {
+                    'get': {
+                        'tags': [sub],
+                        'description': description,
+                        'parameters': [],   # TBD
+                        'responses': {
+                            '200': {
+                                'description': 'OK'
+                            }
+                        }
+                    }
+                }
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+set_mast_openapi_schema()
 
 
 @app.get(root + '{subsystem}/{method}', response_class=PrettyJSONResponse)
