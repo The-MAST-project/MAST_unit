4db45857c3a37afc07ac817bf9b668c6b6f1235f
blumzi <theblumz@gmail.com>
2024-09-19 19:15:55 +0300

    the short version, just  one solve_and_correct, worked

--- src/acquizition.py
+++ src/acquizition.py
@@ -21,6 +21,54 @@ class Acquirer:
     def __init__(self, unit: 'Unit'):
         self.unit: 'Unit' = unit
 
+    def do_solve_and_correct(self, target_ra_j2000_hours: float, target_dec_j2000_degs: float):
+        op = function_name()
+        acquisition_conf = self.unit.unit_conf['acquisition']
+
+        self.unit.start_activity(UnitActivities.Positioning)
+        self.unit.stage.move_to_preset(StagePresetPosition.Sky)
+        self.unit.mount.goto_ra_dec_j2000(target_ra_j2000_hours, target_dec_j2000_degs)
+        while self.unit.stage.is_moving or self.unit.mount.is_slewing:
+            time.sleep(1)
+        logger.info(f"{op}: sleeping 10 seconds to let the mount stop ...")
+        time.sleep(10)
+        self.unit.end_activity(UnitActivities.Positioning)
+
+        # Prepare camera settings
+        acquisition_folder = PathMaker().make_acquisition_folder(
+            tags={'target': f"{target_ra_j2000_hours},{target_dec_j2000_degs}"})
+
+        settings = CameraSettings(
+            seconds=acquisition_conf['exposure'],
+            purpose=ExposurePurpose.Acquisition,
+            base_folder=acquisition_folder,
+            gain=acquisition_conf['gain'],
+            binning=CameraBinning(acquisition_conf['binning']['x'], acquisition_conf['binning']['y']),
+            roi=UnitRoi.from_dict(acquisition_conf['roi']).to_camera_roi(),
+            save=True
+        )
+
+        # Figure out tolerances
+        default_tolerance: Angle = Angle(1 * u.arcsecond)
+        ra_tolerance: Angle = default_tolerance
+        dec_tolerance: Angle = default_tolerance
+        if 'tolerance' in acquisition_conf:
+            if 'ra_arcsec' in acquisition_conf['tolerance']:
+                ra_tolerance = Angle(acquisition_conf['tolerance']['ra_arcsec'] * u.arcsecond)
+            if 'dec_arcsec' in acquisition_conf['tolerance']:
+                dec_tolerance = Angle(acquisition_conf['tolerance']['dec_arcsec'] * u.arcsecond)
+
+        target = Coord(ra=Angle(target_ra_j2000_hours * u.hour), dec=Angle(target_dec_j2000_degs * u.deg))
+
+        if not self.unit.solver.solve_and_correct(exposure_settings=settings,
+                                                  target=target,
+                                                  solving_tolerance=SolvingTolerance(ra_tolerance, dec_tolerance),
+                                                  caller='phase#1',
+                                                  parent_activity=UnitActivities.Acquiring,
+                                                  max_tries=10):
+            logger.info(f"{op}: solve_and_correct failed")
+        logger.info(f"{op}: solve_and_correct done.")
+
     def do_acquire(self, target_ra_j2000_hours: float, target_dec_j2000_degs: float):
         op = function_name()
 
@@ -30,7 +78,7 @@ class Acquirer:
         self.unit.start_activity(UnitActivities.Acquiring)
         self.unit.start_activity(UnitActivities.Positioning)
 
-        logger.info(f"acquisition: phase #1, stage at Sky position")
+        logger.info(f"{op}: phase #1, stage at Sky position")
         #
         # move the stage and mount into position
         #
@@ -40,7 +88,7 @@ class Acquirer:
         self.unit.mount.goto_ra_dec_j2000(target_ra_j2000_hours, target_dec_j2000_degs)
         while self.unit.stage.is_moving or self.unit.mount.is_slewing:
             time.sleep(1)
-        logger.info(f"sleeping 10 seconds to let the mount stop ...")
+        logger.info(f"{op}: sleeping 10 seconds to let the mount stop ...")
         time.sleep(10)
         self.unit.end_activity(UnitActivities.Positioning)
 
@@ -49,6 +97,7 @@ class Acquirer:
         #
         acquisition_folder = PathMaker().make_acquisition_folder(
             tags={'target': f"{target_ra_j2000_hours},{target_dec_j2000_degs}"})
+
         phase1_settings = CameraSettings(
             seconds=acquisition_conf['exposure'],
             purpose=ExposurePurpose.Acquisition,
@@ -87,7 +136,7 @@ class Acquirer:
         #
         # we managed to get within tolerances
         #
-        logger.info(f"acquisition: phase #2, stage at Spec position")
+        logger.info(f"{op}: phase #2, stage at Spec position")
 
         self.unit.stage.move_to_preset(StagePresetPosition.Spec)
         while self.unit.stage.is_moving:
@@ -110,12 +159,16 @@ class Acquirer:
                                                      caller="phase#2",
                                                      parent_activity=UnitActivities.Acquiring,
                                                      max_tries=tries)
-        logger.info("phase #2 (stage at Spec) " + 'succeeded' if success else 'failed')
+        logger.info("{op}: phase #2 (stage at Spec) " + 'succeeded' if success else 'failed')
         if success:
             self.unit.reference_image = self.unit.camera.image
         self.unit.end_activity(UnitActivities.Acquiring)
 
+        logger.info("{op}: >>> We could start guiding now, but not yet! <<<")
         # self.unit.do_guide_by_solving_with_shm(target=target, folder=acquisition_folder)
 
-    def acquire(self, ra_j2000_hours: float, dec_j2000_degs: float):
+    def start_acquisition(self, ra_j2000_hours: float, dec_j2000_degs: float):
         Thread(name='acquisition', target=self.do_acquire, args=[ra_j2000_hours, dec_j2000_degs]).start()
+
+    def start_one_solve_and_correct(self, ra_j2000_hours: float, dec_j2000_degs: float):
+        Thread(name='solve-and-correct', target=self.do_solve_and_correct, args=[ra_j2000_hours, dec_j2000_degs]).start()
